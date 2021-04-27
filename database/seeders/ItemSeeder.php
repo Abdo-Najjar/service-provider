ded2c723012c8b19bbf520f43b97cc91610bdbf4
Abdo Najjar <raf7awe.overlord@gmail.com>
2021-04-27 21:38:10 +0300

    first commit

---
+++ database/seeders/ItemSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Item;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    
+        User::factory()->has(
+            Item::factory()->count(10)
+        )->create();
+
+    }
+
+
+    
+}
